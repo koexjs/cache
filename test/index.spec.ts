4b05825981f5d51264f2ab163aa8daaa6f6e14fe
zero <tobewhatwewant@gmail.com>
2019-01-17 12:10:26 +0800

    :sparkles:(Test): migrate mocha to jest

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,10 @@
 import * as Koa from 'koa';
 import * as request from 'supertest';
-import { expect } from 'chai';
 
 import LRU from '@zcorky/lru';
 import Cache, { Options } from '../src';
 
-const createApp = (options?: Options<string, any>) => {
+const createApp = (options?: Options) => {
   const _app = new Koa();
   _app.use(Cache(options));
   _app.use(async (ctx) => {
@@ -36,109 +35,109 @@ const app = createApp({
 
 describe('Cache Hits', () => {
   it('no cache, hit nothing (0/1)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .get('/')
       .expect(200)
       .expect('hello, world', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['X-Cache-Hits']).to.equal(undefined);
+        expect(res.headers['X-Cache-Hits']).toEqual(undefined);
         done();
       });
   });
 
   it('hit cache (1/2)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .get('/')
       .expect(200)
       .expect('hello, world', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['x-cache-hits']).to.equal('rate=50.00%');
+        expect(res.headers['x-cache-hits']).toEqual('rate=50.00%');
         done();
       });
   });
 
   it('first vist json, miss hit (1/3)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .get('/json')
       .expect(200)
       .expect('{"name":"name","value":"value"}', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['X-Cache-Hits']).to.equal(undefined);
+        expect(res.headers['X-Cache-Hits']).toEqual(undefined);
         done();
       });
   });
 
   it('hit cache (2/4)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .get('/json')
       .expect(200)
       .expect('{"name":"name","value":"value"}', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['x-cache-hits']).to.equal('rate=50.00%');
+        expect(res.headers['x-cache-hits']).toEqual('rate=50.00%');
         done();
       });
   });
 
   it('hit cache (3/5)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .get('/json')
       .expect(200)
       .expect('{"name":"name","value":"value"}', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['x-cache-hits']).to.equal('rate=60.00%');
+        expect(res.headers['x-cache-hits']).toEqual('rate=60.00%');
         done();
       });
   });
 
   it('hit cache (4/6)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .get('/json')
       .expect(200)
       .expect('{"name":"name","value":"value"}', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['x-cache-hits']).to.equal('rate=66.67%');
+        expect(res.headers['x-cache-hits']).toEqual('rate=66.67%');
         done();
       });
   });
 
   it('only cache GET (post)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .post('/json')
       .expect(200)
       .expect('{"name":"name","value":"value"}', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['x-cache-hits']).to.equal(undefined);
+        expect(res.headers['x-cache-hits']).toEqual(undefined);
         done();
       });
   });
 
   it('only cache GET (patch)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .patch('/json')
       .expect(200)
       .expect('{"name":"name","value":"value"}', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['x-cache-hits']).to.equal(undefined);
+        expect(res.headers['x-cache-hits']).toEqual(undefined);
         done();
       });
   });
 
   it('only cache GET (delete)', (done) => {
-    request(app.listen())
+    request(app.callback())
       .delete('/json')
       .expect(200)
       .expect('{"name":"name","value":"value"}', (err, res) => {
         if (err) return done(err);
 
-        expect(res.headers['x-cache-hits']).to.equal(undefined);
+        expect(res.headers['x-cache-hits']).toEqual(undefined);
         done();
       });
   });
